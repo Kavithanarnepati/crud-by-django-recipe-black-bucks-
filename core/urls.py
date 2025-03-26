5ad9f27798b05004386709e6f13b2256efe7563d
unknown <kavithanarnepati@gmail.com>
2025-03-26 20:24:15 +0530

    first commit

--- core/urls.py
+++ core/urls.py
@@ -2,7 +2,7 @@
 URL configuration for core project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.2/topics/http/urls/
+    https://docs.djangoproject.com/en/5.0/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -14,17 +14,13 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path
-from recipe import views
+from django.contrib import admin 
+from django.urls import path 
+from home.views import *
 
-urlpatterns = [
-	path('admin/', admin.site.urls),
-	#path('', views.home),
-	path('add/', views.receipes),
-    path('update_receipe/<int:id>/', views.update_receipe, name='update_receipe'),
-	#path('update/', views.update_receipe),
-path('delete_receipe/<int:id>/', views.delete_receipe, name='delete_receipe'),
-]
-
+urlpatterns = [ 
+	path('', home, name = 'home'), 
+	path('resume/', gen_resume, name = 'resume'), 
+	path("admin/", admin.site.urls), 
+] 
 
